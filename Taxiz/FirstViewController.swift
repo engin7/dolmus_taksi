30827a59f9cc1665e983bb6a72f5b1949a7accad
Engin KUK <kuk.engin@gmail.com>
2020-02-12 12:12:15 +0300

    Initial Commit

---
+++ Taxiz/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Taxiz
+//
+//  Created by Engin KUK on 12.02.2020.
+//  Copyright © 2020 Silverback Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Taxiz/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Taxiz
+//
+//  Created by Engin KUK on 12.02.2020.
+//  Copyright © 2020 Silverback Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
