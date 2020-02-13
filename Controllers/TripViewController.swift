157c700e6c28697d22b4715b92bd7bff0733df23
Engin KUK <kuk.engin@gmail.com>
2020-02-14 01:28:25 +0300

    Tabbar initilaized

--- Controllers/TripViewController.swift
+++ Controllers/TripViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import MapKit
 
 class TripViewController: UIViewController {
-
+    
+ 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager?
+    var currentlocation: CLLocation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.requestLocation()
+        
     }
 
 
 }
+
+ 
+    
+extension TripViewController : CLLocationManagerDelegate {
+     
+    private func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            locationManager?.requestLocation()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        //  will zoom to the first location
+        if let location = locations.first {
+            let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+            let region = MKCoordinateRegion(center: location.coordinate, span: span)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+           
+           print(error.localizedDescription)
+       }
+       
+}
+   
+ 
 
