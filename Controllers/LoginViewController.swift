89ec44987547ae42df236538a2aa411f09e56172
Engin KUK <kuk.engin@gmail.com>
2020-02-15 23:49:54 +0300

    LoginViewController implemented

--- Controllers/LoginViewController.swift
+++ Controllers/LoginViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-//import FireBase
+import Firebase
 
 class LoginViewController: UIViewController {
     
@@ -15,17 +15,85 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var LoginPassword: UITextField!
     
     
+    override func viewDidLoad() {
+       
+       let listener = Auth.auth().addStateDidChangeListener() { auth, user in
+        if user != nil {
+          self.performSegue(withIdentifier: "loginToMap", sender: nil)
+         }
+       }
+       Auth.auth().removeStateDidChangeListener(listener)
+     }
+    
     @IBAction func loginDidTouch(_ sender: AnyObject) {
-        
-        performSegue(withIdentifier: "loginToMap", sender: nil)
+        Auth.auth().signIn(withEmail: LoginEmail.text!, password: LoginPassword.text!)
+         performSegue(withIdentifier: "loginToMap", sender: nil)
     }
     
     
     @IBAction func signUpDidTouch(_ sender: AnyObject) {
+      let alert = UIAlertController(title: "Register",
+                                    message: "Register",
+                                    preferredStyle: .alert)
+      
+      let saveAction = UIAlertAction(title: "Save",
+                                     style: .default) { action in
+      
+         let emailField = alert.textFields![0]
+         let passwordField = alert.textFields![1]
+
+     Auth.auth().createUser(withEmail: emailField.text!, password: passwordField.text!) { user, error in
+           if error != nil {
+            if let errorCode = AuthErrorCode(rawValue: error!._code) {
+              switch errorCode {
+              case .weakPassword:
+                print("please provide strong password")
+              default:
+                print("There is an error")
+                
+              }
+            }
+           }
+          if user != nil {
+            user?.user.sendEmailVerification() {
+              error in
+                print(error?.localizedDescription as Any)
+            }
+             Auth.auth().signIn(withEmail: emailField.text!, password: passwordField.text!)
+            self.performSegue(withIdentifier: "loginToMap", sender: nil)
+          }
+        }
+      }
+      let cancelAction = UIAlertAction(title: "Cancel",
+                                       style: .default)
+      
+      alert.addTextField { textEmail in
+        textEmail.placeholder = "Enter your email"
+      }
+      
+      alert.addTextField { textPassword in
+        textPassword.isSecureTextEntry = true
+        textPassword.placeholder = "Enter your password"
+      }
+      
+      alert.addAction(saveAction)
+      alert.addAction(cancelAction)
+      
+      present(alert, animated: true, completion: nil)
     }
-    
-    
-    
-    
-    
+   
+}
+
+extension LoginViewController: UITextFieldDelegate {
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    if textField == LoginEmail {
+      LoginPassword.becomeFirstResponder()
+    }
+    if textField == LoginPassword {
+      textField.resignFirstResponder()
+    }
+    return true
+  }
+  
 }
