e05aa1d684075e557e077c13e1054e405d0b3a3a
Engin KUK <kuk.engin@gmail.com>
2020-02-23 21:15:14 +0300

    FireStore data OK

--- Controllers/TripsTableViewController.swift
+++ Controllers/TripsTableViewController.swift
@@ -22,103 +22,105 @@ class TripsTableViewCell: UITableViewCell  {
  }
 
 
-class TripsTableViewController: UITableViewController {
+    class TripsTableViewController: UITableViewController {
 
-    @IBAction func chatButtton(_ sender: Any) {
-//        let vc = ChatViewController(currentUser: currentUser!, trip: Trips(time: Date(), to: "Africa", from: "rr", persons: 3))
-//        navigationController?.pushViewController(vc, animated: true)
-        // should go to a list with joined trips
-    }
 
-    private let TripsCellIdentifier = "TripsCell"
-    private var currentTripsAlertController: UIAlertController?
-    
-   
-    private var tripListener: ListenerRegistration?
-   
-    static var trips : [Trips] = []
-
-    deinit {
-      tripListener?.remove()
-    }
-     
-  override func viewDidLoad() {
-    super.viewDidLoad()
- 
-    tripListener = tripReference.addSnapshotListener { querySnapshot, error in
-      guard let snapshot = querySnapshot else {
-        print("Error listening updates: \(error?.localizedDescription ?? "No error")")
-        return
-      }
-        TripsTableViewController.self.trips = []
-        // get data from cloud in array
-         for document in snapshot.documents {
-            let to  = document.get("to") as! String
-            let from = document.get("from") as! String
-            let persons = document.get("persons") as! Int
-            let time = document.get("time") as! Timestamp
-            let newTrip = Trips(time: time.dateValue(), to: to, from: from, persons: persons)
-            TripsTableViewController.self.trips.append(newTrip)
-           }
-        self.tableView.reloadData()
-
-    }
-       
-     }
- 
-    // MARK: - Actions
-    
-    @objc private func signOut() {
-      let ac = UIAlertController(title: nil, message: "Are you sure you want to sign out?", preferredStyle: .alert)
-      ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-      ac.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
-        do {
-          try Auth.auth().signOut()
-        } catch {
-          print("Error signing out: \(error.localizedDescription)")
+      
+        @IBAction func chatButtton(_ sender: Any) {
+    //        let vc = ChatViewController(currentUser: currentUser!, trip: Trips(time: Date(), to: "Africa", from: "rr", persons: 3))
+    //        navigationController?.pushViewController(vc, animated: true)
+            // should go to a list with joined trips
         }
-      }))
-      present(ac, animated: true, completion: nil)
-    }
-   
+
+        private let TripsCellIdentifier = "TripsCell"
+        private var currentTripsAlertController: UIAlertController?
+       
+        private var tripListener: ListenerRegistration?
+        static var trips : [Trips] = []
+
+        deinit {
+          tripListener?.remove()
+        }
+      
+        
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        // listed document and get document with snapshot
+        tripListener = tripReference.addSnapshotListener { querySnapshot, error in
+          guard let snapshot = querySnapshot else {
+            print("Error listening updates: \(error?.localizedDescription ?? "No error")")
+            return
+            }
+            snapshot.documentChanges.forEach { diff in
+            if (diff.type == .added) {
+                let to  = diff.document.get("to") as! String
+                let from = diff.document.get("from") as! String
+                let persons = diff.document.get("persons") as! Int
+                let time = diff.document.get("time") as! Timestamp
+                let newTrip = Trips(time: time.dateValue(), to: to, from: from, persons: persons)
+                TripsTableViewController.self.trips.append(newTrip)
+            }
+            }
+            self.tableView.reloadData()
+        }
  
-    
-    // MARK: UITableView Delegate methods
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return TripsTableViewController.trips.count
-     }
+ 
+        }
+           
      
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
-                 let cell = tableView.dequeueReusableCell(withIdentifier: "TripsTableViewCell", for: indexPath) as! TripsTableViewCell
-        let trip = TripsTableViewController.trips[indexPath.row]
-                 
-        cell.fromTextLabel.text =  trip.from
-        cell.toTextLabel.text = trip.to
-         return cell
-      }
-   
-     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-       return true
-     }
-
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        // MARK: - Actions
         
+        @objc private func signOut() {
+          let ac = UIAlertController(title: nil, message: "Are you sure you want to sign out?", preferredStyle: .alert)
+          ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+          ac.addAction(UIAlertAction(title: "Sign Out", style: .destructive, handler: { _ in
+            do {
+              try Auth.auth().signOut()
+            } catch {
+              print("Error signing out: \(error.localizedDescription)")
+            }
+          }))
+          present(ac, animated: true, completion: nil)
+        }
+       
+     
+        
+        // MARK: UITableView Delegate methods
+
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            return  TripsTableViewController.trips.count
+         }
+         
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+                     let cell = tableView.dequeueReusableCell(withIdentifier: "TripsTableViewCell", for: indexPath) as! TripsTableViewCell
+            let trip =  TripsTableViewController.trips[indexPath.row]
+                     
+            cell.fromTextLabel.text =  trip.from
+            cell.toTextLabel.text = trip.to
+             return cell
+          }
+       
+         override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+           return true
+         }
+
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            
+        }
+        
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+
+            
+            let selectedTrip =  TripsTableViewController.trips[indexPath.row]
+            let vc = ChatViewController(currentUser: currentUser!, trip: selectedTrip)
+            navigationController?.pushViewController(vc, animated: true)
+            
+        }
+        
+      
     }
-    
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-
-        
-        let selectedTrip = TripsTableViewController.trips[indexPath.row]
-        let vc = ChatViewController(currentUser: currentUser!, trip: selectedTrip)
-        navigationController?.pushViewController(vc, animated: true)
-        
-    }
-    
-  
-}
 
 
  
--- Models/Trips.swift
+++ Models/Trips.swift
@@ -10,12 +10,12 @@ import Foundation
 import Firebase
 import UIKit
  
-   let db = Firestore.firestore()
-   var tripReference: CollectionReference {
-   return db.collection("Trips")
-   }
-   
-
+let db = Firestore.firestore()
+      var tripReference: CollectionReference {
+      return db.collection("Trips")
+      }
+      
+ 
 struct Trips {
     
     let id: String?
@@ -32,6 +32,29 @@ struct Trips {
       self.persons = persons
     }
   
+    init?(document: QueryDocumentSnapshot) {
+   
+        let data = document.data()
+ 
+         guard let time = data["time"] as? Date else {
+           return nil
+         }
+         guard let to = data["to"] as? String else {
+           return nil
+         }
+         guard let from = data["from"] as? String else {
+           return nil
+         }
+         guard let persons = data["persons"] as? Int else {
+           return nil
+         }
+         id = document.documentID
+           self.time    = time
+           self.to      = to
+           self.from    = from
+           self.persons = persons
+    
+       }
 }
 
 extension Trips: DatabaseRepresentation {
@@ -53,6 +76,8 @@ extension Trips: DatabaseRepresentation {
   
 }
 
+//- Other extensions
+
 extension Trips: Comparable {
   
   static func == (lhs: Trips, rhs: Trips) -> Bool {
@@ -63,4 +88,21 @@ extension Trips: Comparable {
     return lhs.to < rhs.to
   }
 
+}
+
+protocol DatabaseRepresentation {
+  var representation: [String: Any] { get }
+}
+
+
+extension UIColor {
+  
+  static var primary: UIColor {
+    return UIColor(red: 1 / 255, green: 93 / 255, blue: 48 / 255, alpha: 1)
+  }
+  
+  static var incomingMessage: UIColor {
+    return UIColor(red: 230 / 255, green: 230 / 255, blue: 230 / 255, alpha: 1)
+  }
+  
 }
